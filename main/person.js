f77ab0f3adf37c0b1d96b749621734d437a82ee1
star xiong <313183373@qq.com>
2017-05-28 18:20:51 +0800

    haha

--- main/person.js
+++ main/person.js
@@ -1,2 +1,9 @@
 // Write your code here
-module.exports = Person
+module.exports = function Person(name,age){
+	this.name=name;
+	this.age=age;
+}
+
+Person.prototype.introduce=function(){
+	console.log('My name is '+this.name+'. I am '+this.age+' years old.');
+};
